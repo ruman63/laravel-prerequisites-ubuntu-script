40a8ce89bc7ffbffcd671b90f3af0578c87d8a03
Ruman Saleem <ruman63@gmail.com>
2018-09-03 20:53:50 +0530

    Add Mysql, sqlite & Node to list Also fixes export PATH environment varaiable issue

--- install.sh
+++ install.sh
@@ -1,24 +1,26 @@
 #!/bin/sh
 sudo apt update;
 
-#Install git and curl
-sudo apt install -y git curl;
-#Install PHP & Required PHP Extensions
-sudo apt install -y php php-common php-mbstring php-gd php-mysql php-xml php-zip php-json php-curl php-mcrypt;
-#Install Nginx
-sudo apt install -y nginx;
+#Install git and curl php php-extensions, sqlite, mysql & nginx
+sudo apt install -y git curl php php-common php-mbstring php-gd php-mysql php-sqlite3 php-xml php-zip php-json php-curl php-mcrypt nginx sqlite3 mysql-server;
+
 #Install composer
 curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer;
+
+#Install Node, NPM & Yarn 
+sudo apt install -y nodejs npm 
+sudo npm -g i yarn
+
 #Install laravel cli
 composer global require laravel/installer;
+
 #Add Composer global path
-export PATH=$PATH:$HOME/.config/composer/vendor/bin;
+echo "export PATH=$PATH:$HOME/.config/composer/vendor/bin" | tee -a ~/.bashrc;
 
-
-#Visual Studio Code
+#Install Visual Studio Code
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 sudo apt install apt-transport-https;
 sudo apt update;
-sudo apt install code;
+sudo apt install -y code;
